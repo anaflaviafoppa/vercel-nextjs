106832826f3cb67ab7b6f62bed3ec16a0a2be37f
Ana Foppa <Ana.Foppa@ctw.bmwgroup.com>
2022-12-22 20:43:54 +0000

    add async functions in post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,20 @@
 import Layout from '../../components/Layout/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+
 
 
 export default function Post({ postData }) {
     return <Layout>
-        {postData.title}
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
         <br />
         {postData.id}
         <br />
         {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>;
 }
 
@@ -23,7 +29,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
     // Fetch necessary data for the blog post using params.id
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     return {
         props: {
             postData,
